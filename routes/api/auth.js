902706d1ee408a7b0d65601a1ef3caa7e47e21d1
Jacob-Petersen-1 <jacob.i.petersen52@gmail.com>
2023-02-19 14:37:39 -0700

    Formatted Auth.js + Created Profile Model

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 const dotenv = require("dotenv").config();
-const auth = require('../../middleware/auth')
-const User = require("../../models/User")
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { check, validationResult } = require("express-validator");
@@ -10,67 +10,68 @@ const { check, validationResult } = require("express-validator");
 // @route   GET api/auth
 // @desc    Test route
 // @access  Public
-router.get('/', auth, async (req,res) => { 
-    try {
-        const user = await User.findById(req.user.id).select('-password')
-        res.json(user)
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send('Server Error')
-    }
-    
-    })
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 // @route   POST api/auth
 // @desc    Authenticate USER Return Token
 // @access  Public
 
 router.post(
-    "/",
-    [
-      check("email", "Please include a valid email").isEmail(),
-      check("password","Password is required").exists(),
-    ],
-    async (req, res) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-      }
-  
-      const {email, password } = req.body;
-  
-      try {
-        // See if user exists
-        let user = await User.findOne({ email });
-        if (!user) {
-        return res.status(400).json({ errors: [{ msg: "Invalid Credentials" }] });
-        }
-
-        // Compare Password
-        const isMatch = await bcrypt.compare(password,user.password)
-
-        if(!isMatch){
-        return res.status(400).json({ errors: [{ msg: "Invalid Credentials" }] });
-        }
-
-        // Return jsonwebtoken
-        const payload = {
-          user: {
-            id: user.id,
-          },
-        };
-  
-        jwt.sign(payload, process.env.JWT, { expiresIn: 36000 }, (err, token) => {
-          if (err) throw err;
-          res.json({ token });
-        });
-      } catch (error) {
-        console.error(error.message);
-        res.status(500).send("Server Error");
-      }
+  "/",
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password is required").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-  );
 
+    const { email, password } = req.body;
 
+    try {
+      // See if user exists
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
 
-module.exports = router
+      // Compare Password
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      // Return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(payload, process.env.JWT, { expiresIn: 36000 }, (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+module.exports = router;
