bf514fb6f2fb4ac52a2e530797860886996aea92
Jacob-Petersen-1 <jacob.i.petersen52@gmail.com>
2023-02-23 20:16:26 -0700

    Changed Auth Middleware to Incorporate Better Practice of Auth header Instead of X-AUTH. Tested Protected Endpoints With New Middleware. Successfull Tests.

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,24 +1,28 @@
-const jwt = require('jsonwebtoken')
-const config = require('config')
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const dotenv = require("dotenv").config();
 
-module.exports = function(req, res, next) {
-    // Get Token from header
-    const token = req.header('x-auth-token');
+module.exports = function (req, res, next) {
+  // Get token from header
+  const bearerHeader = req.headers["authorization"];
 
-    // Check if no token
-    if(!token) {
-        return res.status(401).json({msg: "No token, authorization denied"})
+  // Check if bearer is undefined
+  if (typeof bearerHeader !== "undefined") {
+    //split the space at the bearer
+    const bearer = bearerHeader.split(" ");
+    // Get token from string
+    const bearerToken = bearer[1];
 
-    }
-
+    req.token = bearerToken;
     try {
-        const decoded = jwt.verify(token, process.env.JWT);
-
-        req.user = decoded.user;
-        next();
+      const decoded = jwt.verify(bearerToken, process.env.JWT);
+      req.user = decoded.user;
+      next();
     } catch (error) {
-        res.status(401).json({msg: 'Token is not valid'})
-    }
+        res.status(401).json({ msg: "Token is not valid" });
 
-}
+    }
+  } else {
+    res.status(403).json({ msg: "Token is not valid" });
+  }
+};
