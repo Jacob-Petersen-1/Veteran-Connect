4fa537c88854146ca4390b53e9183cbcc2c00952
Jacob-Petersen-1 <jacob.i.petersen52@gmail.com>
2023-02-27 17:26:51 -0700

    Created Hooks For Authentication + Created Base Page Components using React Router + TODO Testing Auth Hooks with form/Create Form Hook + Test Authentication.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
-
+// General Imports
+import {Routes,Route} from "react-router-dom"
 import './App.css';
+import HomePage from "./pages/HomePage/HomePage";
+
+//Pages Imports
+import LandingPage from "./pages/LandingPage/LandingPage";
+
+
+//Utils Imports
+import PrivateRoute from "./utils/PrivateRoute";
 
 function App() {
   return (
-    <div>
+    <>
+    <Routes>
+      <Route path="/" element={LandingPage} />
+      <Route path="/home" element={<PrivateRoute> <HomePage/></PrivateRoute>} />
+    </Routes>
+    </>
+    
 
-    </div>
+    
   );
 }
 
---
+++ client/src/pages/LandingPage/LandingPage.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+
+
+
+const LandingPage = () => {
+    return ( 
+    <div>
+        <h1>Landing Page</h1>
+    </div> );
+}
+ 
+export default LandingPage;
