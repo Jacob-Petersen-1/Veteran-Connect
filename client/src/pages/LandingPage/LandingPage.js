2e0926f277134aa150bd2335f9e8d400686d2fb0
Jacob-Petersen-1 <jacob.i.petersen52@gmail.com>
2023-03-01 10:45:32 -0700

    Clean Slated Components To Further Develop Practical Design Patters And Further Learning Using MUI Components For A Mobile Response Application + Good Color Paletes

--- client/src/pages/LandingPage/LandingPage.js
+++ client/src/pages/LandingPage/LandingPage.js
@@ -1,25 +1,12 @@
 import React from "react";
-import LoginForm from "../../components/LoginForm/LoginForm";
-import AppBarLanding from "../../components/AppBarLanding/AppBarLanding";
+
 
 //MUI imports
-import { Button, Container, Paper, Typography,Box } from "@mui/material";
+
 
 const LandingPage = () => {
   return (
-    <Container
-      sx={{
-        height: "100vh",
-        backgroundImage:
-          "url(https://images.unsplash.com/photo-1604339454409-701c5278c546?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NXxGcUY2VHl1NHoyUXx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60)",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-      }}
-    >
-      <AppBarLanding />
-     
-      
-    </Container>
+ <h1>Landing</h1>
   );
 };
 
